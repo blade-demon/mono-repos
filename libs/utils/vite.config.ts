ce2c4038af2862901a8d1dc5dfe6262b19e3d1e3
徐紫微 <xuziwei89@gmail.com>
2024-07-10 00:33:50 +0800

    add new lib project

--- libs/utils/vite.config.ts
+++ libs/utils/vite.config.ts
@@ -1,15 +1,8 @@
-// import { defineConfig } from 'vite';
-// import path from 'path';
-// import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
-// // Ensure __dirname is available in ESM
-// import { fileURLToPath } from 'url';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// export default defineConfig({
-//   build: {
-//     lib: { entry: path.resolve(__dirname, 'src/main.ts'), name: 'utils' },
-//   },
-//   plugins: [dts()],
-// });
+export default defineConfig({
+  build: { lib: { entry: resolve(__dirname, 'src/main.ts'), name: 'utils' } },
+  plugins: [dts()],
+});
